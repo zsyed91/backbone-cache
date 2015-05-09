7fc4d6d074ac9bdd901a40497e3172f18df4310a
Zshawn Syed <zsyed91@gmail.com>
2015-05-09 18:18:18 +0100

    Project init

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    // Project configuration.
+    grunt.initConfig({
+        jasmine : {
+            src : 'src/**/*.js',
+            options : {
+                specs : 'spec/**/*.js',
+                template: require('grunt-template-jasmine-requirejs'),
+                templateOptions: {
+                    requireConfigFile: 'lib/require/main.js'
+                }
+            }
+        },
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'src/**/*.js',
+                'spec/**/*.js'
+            ],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    grunt.registerTask('test', ['jshint', 'jasmine']);
+
+    grunt.registerTask('default', ['test']);
+
+};
